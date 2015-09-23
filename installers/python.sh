1f35646de5f7343ef80241795337f8125c6d20c0
jmsdnns <jdennis@gmail.com>
2015-09-23 12:24:47 -0400

    Ack. A string was not closed properly

--- installers/python.sh
+++ installers/python.sh
@@ -7,7 +7,7 @@ apt-get -y install \
 echo ""
 echo "Q: please add the following lines to $HOME/.profile"
 echo ""
-echo "# Virtualenv
+echo "# Virtualenv"
 echo "export WORKON_HOME=\"$HOME/.virtualenvs\""
 echo ". /usr/local/bin/virtualenvwrapper.sh"
 echo ""
