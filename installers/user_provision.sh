6f9edd484df8cccb84022a731787aea042cc83e6
coppersmith <glen@qntfy.io>
2015-10-28 12:27:16 -0400

    Added a place for when the box will be used for e.g., dev

---
+++ installers/user_provision.sh
@@ -0,0 +1,12 @@
+### Update package manager and packages
+sudo apt-get -qq update
+
+
+### Headless install
+sudo env DEBIAN_FRONTEND=noninteractive apt-get -o DPkg::Options::=--force-confnew upgrade -q -y < /dev/null
+
+
+### Install universal needs
+sudo apt-get -y install \
+    emacs \
+    tmux
